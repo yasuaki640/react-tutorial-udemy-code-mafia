1d3b4c371e83f9ec6626cf19b708013c59cc25c1
yasuaki640 <yasuaki640@gmail.com>
2024-04-11 08:51:45 +0900

    Refactor code to use consistent quotation marks and spacing in main.tsx and App.tsx files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
       <h1>Code Mafia React Tutorial Codes.</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {EventListenerState} from "./pages/EventListenerState.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { EventListenerState } from "./pages/EventListenerState.tsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/event-listener-state",
-        element: <EventListenerState />
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/event-listener-state",
+    element: <EventListenerState />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
