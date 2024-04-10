c62c316ad0746cbe962f51583cb1a9562efc2418
yasuaki640 <yasuaki640@gmail.com>
2024-04-10 21:39:24 +0900

    routerいれる

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {EventListenerState} from "./pages/EventListenerState.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/event-listener-state",
+        element: <EventListenerState />
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/EventListenerState.tsx
@@ -0,0 +1,3 @@
+export const EventListenerState = () => {
+  return <div>hello.</div>
+}
