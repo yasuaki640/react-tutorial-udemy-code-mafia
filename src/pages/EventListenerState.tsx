dc51448b74faaedc99913883234635573f965e65
yasuaki640 <yasuaki640@gmail.com>
2024-04-11 09:01:44 +0900

    Refactor code to use consistent quotation marks and spacing in main.tsx and App.tsx files.

--- src/pages/EventListenerState.tsx
+++ src/pages/EventListenerState.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export const EventListenerState = () => {
-  return <div>hello.</div>
-}
+  const [val, setVal] = useState<string>();
+
+  console.log("Re rendering done!!!");
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setVal(e.target.value);
+        }}
+      />
+      <span> = {val}</span>
+    </>
+  );
+};
