da9f30b8dfec831e47849c5fa7dbc79c2b5a0710
yasuaki640 <yasuaki640@gmail.com>
2024-04-15 08:43:58 +0900

    Refactor ControlSyntax component to use useState hook for filtering animals array based on input value.

--- src/pages/ControlSyntax.tsx
+++ src/pages/ControlSyntax.tsx
@@ -1,43 +1,25 @@
-import React from "react";
+import { useState } from "react";
 
-type ProfileProps = {
-  name: string;
-  age: number;
-};
-
-const Profile: React.FC<ProfileProps> = ({ name, age }) => {
-  return (
-    <div>
-      <h3>Profile</h3>
-      <ul>
-        <li>Name : {name}</li>
-        <li>Age : {age}</li>
-      </ul>
-    </div>
-  );
-};
-
-const profiles: ProfileProps[] = [
-  { name: "yasu", age: 123 },
-  { name: "taro", age: 456 },
-  { name: "hanako", age: 789 },
-];
+const animals = ["dog", "cat", "bird"];
 
 export const ControlSyntax = () => {
+  const [filterKeyword, setFilterKeyword] = useState("");
+
   return (
     <>
-      <h3>練習問題</h3>
-      <p>
-        Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。
-      </p>
-      <p>
-        また、Profileコンポーネント内のリスト表示部分のkeyを設定して、ワーニング表示がされないようにしてください。
-      </p>
-      <div>
-        {profiles.map((p) => (
-          <Profile key={p.name + p.age} {...p} />
-        ))}
-      </div>
+      <h3>配列のfilterメソッド</h3>
+      <input
+        type="text"
+        value={filterKeyword}
+        onChange={(e) => setFilterKeyword(e.target.value)}
+      />
+      <ul>
+        {animals
+          .filter((a) => a.indexOf(filterKeyword) !== -1)
+          .map((a) => (
+            <li key={a}>{a}</li>
+          ))}
+      </ul>
     </>
   );
 };
