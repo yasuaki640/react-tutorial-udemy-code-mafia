14ff43aa749c213bd3653057681d5735a8d539c6
yasuaki640 <yasuaki640@gmail.com>
2024-04-14 19:03:10 +0900

    Added new component EventListenerState0412_exercise_2 and its route
    This commit introduces a new component, `EventListenerState0412_exercise_2`, which has functionality to shuffle numbers in an array. Along with this new component, a corresponding route is added to the application's router, making the component accessible within the application.

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { EventListenerState } from "./pages/EventListenerState.tsx";
 import { EventListenerState0412 } from "./pages/EventListenerState0412.tsx";
 import { EventListenerState0412_2 } from "./pages/EventListenerState0412_2.tsx";
 import { EventListenerState0412_exercise } from "./pages/EventListenerState0412_exercise.tsx";
+import { EventListenerState0412_exercise_2 } from "./pages/EventListenerState0412_exercise_2.tsx";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
   {
     path: "/event-listener-state0412_exercise",
     element: <EventListenerState0412_exercise />,
+  },
+  {
+    path: "/event-listener-state0412_exercise_2",
+    element: <EventListenerState0412_exercise_2 />,
   },
 ]);
 
