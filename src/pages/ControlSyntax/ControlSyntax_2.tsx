5d0f573be9fc4d2ee5363205b8a291f85c88b3f1
yasuaki640 <yasuaki640@gmail.com>
2024-04-17 23:25:16 +0900

    Fixed radio button options and added sum calculation functionality.

--- src/pages/ControlSyntax/ControlSyntax_2.tsx
+++ src/pages/ControlSyntax/ControlSyntax_2.tsx
@@ -2,34 +2,29 @@ import { useState } from "react";
 
 const animals: string[] = ["dog", "cat", "bird"];
 
-export const ControlSyntax_2 = () => {
-  const [maxim, setMaxim] = useState("あまり強い言葉を使うなよ 弱く見えるぞ");
-  const onChange = (e) => setMaxim(e.target.value);
+type Maxim = {
+  label: string;
+  名言度: number;
+  checked: boolean;
+};
 
-  const maximOptions = [
-    "月島さんのおかげ",
-    "一体いつからフルリモート勤務だと錯覚していた?",
-    "命を刈り取る、形をしてるだろ？",
-  ];
+export const ControlSyntax_2 = () => {
+  const [maxim, setMaxim] = useState<Maxim[]>([
+    { label: "月島さんのおかげ", 名言度: 100, checked: false },
+    {
+      label: "一体いつからフルリモート勤務だと錯覚していた?",
+      名言度: 100,
+      checked: false,
+    },
+    { label: "命を刈り取る、形をしてるだろ？", 名言度: 100, checked: false },
+  ]);
+
+  const [sum, setSum] = useState(0);
 
   return (
     <>
-      <h2>fuck dip</h2>
-      <p>Selected is 「{maxim}」</p>
       <div>
-        {maximOptions.map((option) => (
-          <div key={option}>
-            <label>
-              <input
-                type="radio"
-                value={option}
-                checked={option === maxim}
-                onChange={onChange}
-              />
-              {option}
-            </label>
-          </div>
-        ))}
+        <div>私の攻撃力は : {sum}</div>
       </div>
     </>
   );
